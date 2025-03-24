9fb397b4eac2a681349806c8f92fe56b5780938a
Flingbot Tech <flingbottech@gmail.com>
2025-03-24 06:04:10 -0400

    Range sensor filtering

--- src/main/java/botsnax/range/MountedRangeSensor.java
+++ src/main/java/botsnax/range/MountedRangeSensor.java
@@ -4,16 +4,23 @@ import edu.wpi.first.math.geometry.Transform2d;
 import edu.wpi.first.math.geometry.Translation2d;
 
 import java.util.Optional;
+import java.util.function.Function;
 
 import static edu.wpi.first.units.Units.Meters;
 
 public class MountedRangeSensor {
     private final RangeSensor rangeSensor;
     private final Transform2d robotToSensor;
+    private final Function<Double, Double> filter;
 
     public MountedRangeSensor(RangeSensor rangeSensor, Transform2d robotToSensor) {
+        this(rangeSensor, robotToSensor, Function.identity());
+    }
+
+    public MountedRangeSensor(RangeSensor rangeSensor, Transform2d robotToSensor, Function<Double, Double> filter) {
         this.rangeSensor = rangeSensor;
         this.robotToSensor = robotToSensor;
+        this.filter = filter;
     }
 
     public RangeSensor getSensor() {
@@ -26,7 +33,8 @@ public class MountedRangeSensor {
 
     public Optional<PositionEstimate> getFieldRelativeMeasurementIfAny() {
         return rangeSensor.getDistance().map(measurement -> {
-            Translation2d fieldPoint = new Translation2d(measurement.distance().in(Meters), 0)
+            double distance = filter.apply(measurement.distance().in(Meters));
+            Translation2d fieldPoint = new Translation2d(distance, 0)
                     .rotateBy(robotToSensor.getRotation())
                     .plus(robotToSensor.getTranslation())
                     ;
